773ca775fdea172275647d5cf3138047d8276f2e
phibkro <71797726+phibkro@users.noreply.github.com>
2024-07-23 20:24:00 +0200

    fix: play walk animation regardless of velocity
    Walk animation did not play if velocity == 0.
    Changed so walk animation is dependent on control inputs rather than
    velocity.

--- src/characters/player.gd
+++ src/characters/player.gd
@@ -15,34 +15,38 @@ func _ready():
 func _process(delta):
 	if not isAlive:
 		return
-	
 	var velocity = Vector2.ZERO
+	var isMoving = false
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 		sprite.flip_h = false
+		isMoving = true
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 		sprite.flip_h = true
+		isMoving = true
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+		isMoving = true
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		isMoving = true
 	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	if isMoving:
 		sprite.play("walk")
 	else:
 		sprite.play("idle")
-	
+
+	velocity = velocity.normalized() * speed
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 
 func _on_body_entered(body):
 	isAlive = false
 	sprite.play("death")
-	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)
+	hit.emit()
 
 func start(pos):
 	isAlive = true
